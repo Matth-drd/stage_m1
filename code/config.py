f7c0347ca059242b28b2911569c7fee1613a863f
mdurand <ID+username@users.noreply.github.com>
2026-05-22 11:14:41 +0200

    maj confi

--- code/config.py
+++ code/config.py
@@ -5,10 +5,11 @@
 # import config as conf
 
 split_ratio = .8
+poids_but = 2
 path_clean = '../data/csv/foot_v4.csv'
 path_clean_cell = 'data/csv/foot_v4.csv'
 
-targets = ["FTR", "Hvs", 'Avs', 'Dvs']
+targets = ["FTR", "Hvs", 'Avs']
 
 features = [
     'FT_Hforme', 'FT_Aforme', 'FT_Hatt', 'FT_Aatt', 'FT_Hdef', 'FT_Adef',
@@ -24,6 +25,7 @@ features = [
     'FT_prec_weight_ratio', 'FT_Elo_ratio', 'HT_Elo_ratio', 'FT_avgF_ratio',
     'FT_avgY_ratio', 'FT_avgR_ratio', 'H_WinStreak', 'A_WinStreak',
     'H_LoseStreak', 'A_LoseStreak', 'WinStreak_diff', 'LoseStreak_diff', 'H_Rank', 'A_Rank', 'Rank_diff']
+
 # print(len(features))
 # grace au random forest, on trouve les features classées par importance.
 # On choisis arbitrairement les 15 premières pour faire les modèles.
@@ -35,9 +37,9 @@ features = [
 Utilisation de RFECV, SelectKBest, f_classif, mutual_info_classif pour 
 séléctionner les meilleurs features, total de 12 features retenues.
 """
-features_importante = [
-    'FT_Aforme', 'FT_Elo_A', 'FT_Elo_H', 'FT_Elo_dif', 'FT_Hforme', 'FT_forme_diff',
-    'FT_forme_ratio', 'HT_Elo_H', 'HT_Elo_dif', 'HT_Elo_ratio', 'HT_forme_diff', 'HT_forme_ratio']
+# features_importante = [
+#     'FT_Aforme', 'FT_Elo_A', 'FT_Elo_H', 'FT_Elo_dif', 'FT_Hforme', 'FT_forme_diff',
+#     'FT_forme_ratio', 'HT_Elo_H', 'HT_Elo_dif', 'HT_Elo_ratio', 'HT_forme_diff', 'HT_forme_ratio']
 
 # ['FT_Hprecision', 'FT_Hprec_weight', 'FT_Aprec_weight', 'FT_prec_diff', 'FT_prec_weight_diff',
 #  'FT_Elo_H',
@@ -71,6 +73,6 @@ ft_commune = [
     'HT_Hforme', 'HT_def_ratio', 'HT_forme_diff', 'HT_forme_ratio', 'Rank_diff']
 # pour NN
 ECD = ['FT_Aprec_weight', 'FT_avgY_diff', 'HT_Elo_A', 'HT_Elo_dif', 'HT_Hdef', 'WinStreak_diff']
-
+ft_nn = ECD
 betting = ['B365H', 'B365D', 'B365A']
 
